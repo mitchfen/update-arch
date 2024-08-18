b5ade75c3e81a05e730c928e0163d34b2cb77215
Mitchell Fenner <mitchfen@pm.me>
2024-08-18 18:53:37 -0400

    feat: flatpak updates, bios script, package counts

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func writeSection(msg string) {
 	color := "\033[35m"  // Magenta color code
 	color2 := "\033[36m" // Cyan color code
 	divider := strings.Repeat("─", 80)
+	fmt.Println("")
 	fmt.Print(color) // Change color without showing anything
 	fmt.Println(divider)
 	fmt.Print(color2) // Change color without showing anything
@@ -23,32 +24,54 @@ func runCommand(command string, args ...string) (string, error) {
 	cmd := exec.Command(command, args...)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return "", fmt.Errorf("error running command: %v", err)
+		return "", fmt.Errorf("error running command: %v \n Output: %s", err, out)
 	}
 	return string(out), nil
 }
 
 func main() {
-	//scriptsPath := "~/dev/scripts_and_configs/scripts"
 
 	writeSection("Updating pacman packages...")
-	commandOutput, err := runCommand("sudo", "pacman", "-Syu")
-	fmt.Println(commandOutput)
+	commandOutput, err := runCommand("sudo", "pacman", "-Syu", "--noconfirm")
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(commandOutput)
 
 	writeSection("Updating flatpak packages...")
-	commandOutput, err = runCommand("flatpak", "update")
-	fmt.Println(commandOutput)
+	commandOutput, err = runCommand("flatpak", "update", "--force")
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(commandOutput)
+
+	writeSection("Removing orphan pacman packages...")
+	commandOutput, _ = runCommand("sudo", "pacman", "-Qdt", "--noconfirm")
+	if commandOutput != "" {
+		commandOutput, err = runCommand("sudo", "pacman", "-Rns", commandOutput)
+	} else {
+		fmt.Println("No orphan packages to remove.")
+	}
+
+	writeSection("Checking for latest BIOS version...")
+	commandOutput, err = runCommand("python", "/home/mitchfen/dev/scripts_and_configs/scripts/getLatestBiosVersion.py")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(commandOutput)
+
+	writeSection("Getting current package counts...")
+	commandOutput, err = runCommand("sudo", "pacman", "-Q")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Pacman packages: %d", strings.Count(commandOutput, "\n"))
+	commandOutput, err = runCommand("flatpak", "list")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("\nFlatpak packages: %d", strings.Count(commandOutput, "\n"))
 
 	// TODO: Update aur packages using git pull
 	// TODO: Update ohmyposh by after checking ~/.oh-my-posh-version.txt
-	// TODO: Remove unused pacman packages
-	// TODO: Run python script to check for BIOS update
-	// TODO: Print out latest package counts
-
 }
