fc85275cd298261cfbdd35e2e78b8863914ba5e6
Mitchell Fenner <mitchfen@pm.me>
2024-08-19 14:47:31 -0400

    feat: aur package update function

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -10,6 +11,7 @@ func WriteSection(msg string) {
 	color := "\033[35m"  // Magenta color code
 	color2 := "\033[36m" // Cyan color code
 	divider := strings.Repeat("─", 80)
+	fmt.Print("\n")  // Change color without showing anything
 	fmt.Print(color) // Change color without showing anything
 	fmt.Println(divider)
 	fmt.Print(color2) // Change color without showing anything
@@ -26,4 +28,55 @@ func RunCommand(command string, args ...string) (string, error) {
 		return "", fmt.Errorf("error running command: %v \n Output: %s", err, out)
 	}
 	return string(out), nil
+}
+
+func UpdateAurPackages(aurPath string) error {
+	err := os.Chdir(aurPath)
+	if err != nil {
+		return fmt.Errorf("failed to change to AUR directory: %w", err)
+	}
+
+	dirEntries, err := os.ReadDir(aurPath)
+	if err != nil {
+		return fmt.Errorf("failed to read directory: %w", err)
+	}
+
+	// Iterate over subdirectories
+	for _, dirEntry := range dirEntries {
+		if dirEntry.IsDir() {
+			err = os.Chdir(dirEntry.Name())
+			if err != nil {
+				return fmt.Errorf("failed to change directory: %w", err)
+			}
+
+			// Execute `git pull`
+			gitOutput, err := RunCommand("git", "pull")
+			if err != nil {
+				return fmt.Errorf("failed to execute git pull: %w", err)
+			}
+
+			// Check if the repository is up to date
+			if strings.Contains(gitOutput, "Already up to date.") {
+				fmt.Printf("%s is already up to date\n", dirEntry.Name())
+			} else {
+				fmt.Printf("%s is being updated...\n", dirEntry.Name())
+				_, err = RunCommand("makepkg", "-si")
+				if err != nil {
+					return fmt.Errorf("failed to execute makepkg -si: %w", err)
+				}
+
+				// Execute `git clean -fxd`
+				err = exec.Command("git", "clean", "-fxd").Run()
+				if err != nil {
+					return fmt.Errorf("failed to execute git clean -fxd: %w", err)
+				}
+			}
+
+			err = os.Chdir(aurPath)
+			if err != nil {
+				return fmt.Errorf("failed to change back to parent directory: %w", err)
+			}
+		}
+	}
+	return err
 }
--- main.go
+++ main.go
@@ -6,16 +6,12 @@ import (
 	"strings"
 )
 
+const biosVersionScriptPath = "/home/mitchfen/dev/scripts_and_configs/scripts/getLatestBiosVersion.py"
+const aurPath = "/home/mitchfen/aur"
+
 func main() {
 	helpers.WriteSection("Updating pacman packages...")
 	commandOutput, err := helpers.RunCommand("sudo", "pacman", "-Syu", "--noconfirm")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(commandOutput)
-
-	helpers.WriteSection("Updating flatpak packages...")
-	commandOutput, err = helpers.RunCommand("flatpak", "update", "--noninteractive")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -29,12 +25,25 @@ func main() {
 		fmt.Println("No orphan packages to remove.")
 	}
 
-	helpers.WriteSection("Checking for latest BIOS version...")
-	commandOutput, err = helpers.RunCommand("python", "/home/mitchfen/dev/scripts_and_configs/scripts/getLatestBiosVersion.py")
+	helpers.WriteSection("Updating flatpak packages...")
+	commandOutput, err = helpers.RunCommand("flatpak", "update", "--noninteractive")
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(commandOutput)
+
+	helpers.WriteSection("Checking for latest BIOS version...")
+	commandOutput, err = helpers.RunCommand("python", biosVersionScriptPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(commandOutput)
+
+	helpers.WriteSection("Updating AUR packages...")
+	err = helpers.UpdateAurPackages(aurPath)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	helpers.WriteSection("Getting current package counts...")
 	commandOutput, err = helpers.RunCommand("sudo", "pacman", "-Q")
@@ -48,6 +57,5 @@ func main() {
 	}
 	fmt.Printf("\nFlatpak packages: %d", strings.Count(commandOutput, "\n"))
 
-	// TODO: Update aur packages using git pull
 	// TODO: Update ohmyposh by after checking ~/.oh-my-posh-version.txt
 }
